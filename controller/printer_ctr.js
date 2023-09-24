01399f7db4789b1158ff0b600eddf6a02f216d58
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-09-24 15:13:13 +0500

    added put search delete methods to printer table

--- controller/printer_ctr.js
+++ controller/printer_ctr.js
@@ -45,7 +45,91 @@ const getPrinters = async (req, res) => {
   }
 };
 
+const deletePrinter = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const foundedId = await Printer.findOne({ where: { id: id } });
+
+    if (!foundedId) {
+      return res.send({
+        msg: "id not found",
+      });
+    }
+
+    if (!id) {
+      return res.send({
+        msg: "id required",
+      });
+    }
+    await Printer.destroy({
+      returning: true,
+      plain: true,
+      where: {
+        id,
+      },
+    });
+    return res.send({
+      msg: "deleted printer!",
+    });
+  } catch (error) {
+    return res.send({
+      msg: error.message,
+    });
+  }
+};
+
+const search = async (req, res) => {
+  try {
+    const { search } = req.headers;
+
+    let searchValidation = search.trim();
+
+    let objIsmsharif = await Printer.findAll({
+      where: { title: searchValidation },
+    });
+    return res.status(201).send(objIsmsharif);
+  } catch (err) {
+    return res.send({ msg: err.message });
+  }
+};
+
+const updatePrinter = async (req, res) => {
+  try {
+    const { title, comments, price, brand, category, picture, sinfi } = req.body;
+    const { id } = req.params;
+
+    const updatedComponent = await Printer.update(
+      {
+        title,
+        comments,
+        price,
+        brand,
+        category,
+        picture,
+        sinfi
+      },
+      {
+        returning: true,
+        plain: false,
+        where: {
+          id,
+        },
+      }
+    );
+
+    return res.send(updatedComponent.filter((e) => e));
+  } catch (error) {
+    return res.send({
+      msg: error.message,
+    });
+  }
+};
+
 module.exports = {
   createPrinter,
   getPrinters,
+  search,
+  updatePrinter,
+  deletePrinter
 };
--- router/printer_router.js
+++ router/printer_router.js
@@ -1,9 +1,12 @@
 const { Router } = require("express")
-const {createPrinter, getPrinters} = require('../controller/printer_ctr')
+const {createPrinter, getPrinters,search, deletePrinter, updatePrinter} = require('../controller/printer_ctr')
 
 const printerRouter = Router()
 
 printerRouter.post("/create_printer", createPrinter)
 printerRouter.get('/get_printers', getPrinters)
+printerRouter.get("/search_printer", search)
+printerRouter.delete("/delete_printer/:id", deletePrinter)
+printerRouter.put("/update_printer/:id", updatePrinter)
 
 module.exports = printerRouter
