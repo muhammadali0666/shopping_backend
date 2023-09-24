132cd7003202d8389d279da62ef433057a46863c
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-09-24 10:49:45 +0500

    added put delete search apis

--- controller/computerComponents_ctr.js
+++ controller/computerComponents_ctr.js
@@ -12,7 +12,7 @@ const createComponent = async (req, res) => {
       price,
       brand,
       category,
-      picture
+      picture,
     });
 
     return res.status(200).send({
@@ -36,7 +36,72 @@ const getComponents = async (req, res) => {
   }
 };
 
+const deleteComponent = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    //  await ComputerComponents.findOne({ where: { id: id } })
+
+    await ComputerComponents.destroy({
+      returning: true,
+      plain: true,
+      where: {
+        id,
+      },
+    });
+    return res.send("deleted component!");
+  } catch (error) {
+    return res.send({
+      msg: error.message,
+    });
+  }
+};
+
+const search = async (req, res) => {
+  try {
+    const { search } = req.headers;
+
+    let searchToLowerCase = search.toLowerCase();
+
+    let searchValidation = searchToLowerCase.trim();
+
+    let objIsmsharif = await ComputerComponents.findAll({
+      where: { title: searchValidation },
+    });
+    return res.status(201).send(objIsmsharif);
+  } catch (err) {
+    return res.send({ msg: err.message });
+  }
+};
+
+const updateComponent = async (req, res) => {
+  try {
+    const { title, price, comments, brand, category } = req.body;
+    const { id } = req.params;
+
+    const updatedComponent = await ComputerComponents.update(
+      { title, price, comments, brand, category },
+      {
+        returning: true,
+        plain: false,
+        where: {
+          id,
+        },
+      }
+    );
+
+    return res.send(updatedComponent.filter((e) => e));
+  } catch (error) {
+    return res.send({
+      msg: error.message,
+    });
+  }
+};
+
 module.exports = {
   createComponent,
   getComponents,
+  deleteComponent,
+  search,
+  updateComponent,
 };
--- router/computerComponents_router.js
+++ router/computerComponents_router.js
@@ -1,9 +1,12 @@
 const { Router } = require("express")
-const {createComponent, getComponents} = require('../controller/computerComponents_ctr')
+const {createComponent, getComponents, deleteComponent, search, updateComponent} = require('../controller/computerComponents_ctr')
 
 const componentRouter = Router()
 
 componentRouter.post("/create_component", createComponent)
 componentRouter.get('/get_components', getComponents)
+componentRouter.delete("/delete_component/:id", deleteComponent)
+componentRouter.get("/search_component", search)
+componentRouter.put("/update_component/:id", updateComponent)
 
 module.exports = componentRouter
